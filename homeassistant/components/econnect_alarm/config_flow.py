e481b21759074d5690070379f22b1884a35f55d1
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2021-03-12 18:34:10 +0100

    feat(config): improve setup flow; suggest previously inserted values

--- homeassistant/components/econnect_alarm/config_flow.py
+++ homeassistant/components/econnect_alarm/config_flow.py
@@ -22,15 +22,6 @@ from .helpers import parse_areas_config
 _LOGGER = logging.getLogger(__name__)
 
 
-STEP_USER_DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_USERNAME): str,
-        vol.Required(CONF_PASSWORD): str,
-        vol.Optional(CONF_DOMAIN, default=""): str,
-    }
-)
-
-
 class EconnectConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for E-connect Alarm."""
 
@@ -45,31 +36,45 @@ class EconnectConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial configuration."""
-        if user_input is None:
-            return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
-            )
-
         errors = {}
+        if user_input is not None:
+            try:
+                # Validate submitted configuration
+                await validate_credentials(self.hass, user_input)
+                await validate_areas(self.hass, user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except InvalidAuth:
+                errors["base"] = "invalid_auth"
+            except InvalidAreas:
+                errors["base"] = "invalid_areas"
+            except Exception as err:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception", err)
+                errors["base"] = "unknown"
+            else:
+                return self.async_create_entry(title="E-connect Alarm", data=user_input)
 
-        try:
-            # Validate submitted configuration
-            await validate_credentials(self.hass, user_input)
-            await validate_areas(self.hass, user_input)
-        except CannotConnect:
-            errors["base"] = "cannot_connect"
-        except InvalidAuth:
-            errors["base"] = "invalid_auth"
-        except InvalidAreas:
-            errors["base"] = "invalid_areas"
-        except Exception as err:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception", err)
-            errors["base"] = "unknown"
-        else:
-            return self.async_create_entry(title="E-connect Alarm", data=user_input)
-
+        # Populate with latest changes
+        user_input = user_input or {}
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_USERNAME,
+                        description={"suggested_value": user_input.get(CONF_USERNAME)},
+                    ): str,
+                    vol.Required(
+                        CONF_PASSWORD,
+                        description={"suggested_value": user_input.get(CONF_PASSWORD)},
+                    ): str,
+                    vol.Optional(
+                        CONF_DOMAIN,
+                        description={"suggested_value": user_input.get(CONF_DOMAIN)},
+                    ): str,
+                }
+            ),
+            errors=errors,
         )
 
 
@@ -146,7 +151,7 @@ async def validate_credentials(hass: core.HomeAssistant, data):
 
     try:
         # Check Credentials
-        client = ElmoClient(BASE_URL, domain=data[CONF_DOMAIN])
+        client = ElmoClient(BASE_URL, domain=data.get(CONF_DOMAIN))
         return await hass.async_add_executor_job(
             client.auth, data[CONF_USERNAME], data[CONF_PASSWORD]
         )
