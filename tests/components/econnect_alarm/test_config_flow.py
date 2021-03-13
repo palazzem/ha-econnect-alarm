c7fcb9328705e3dd46de38a96f66938a8d266905
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2021-03-13 17:20:14 +0100

    test: `config_flow` form is tested; test submit with success initializes `ElmoClient`

--- tests/components/econnect_alarm/test_config_flow.py
+++ tests/components/econnect_alarm/test_config_flow.py
@@ -1,94 +1,55 @@
 """Test the E-connect Alarm config flow."""
 from unittest.mock import patch
 
-from homeassistant import config_entries, setup
-from homeassistant.components.econnect_alarm.config_flow import (
-    CannotConnect,
-    InvalidAuth,
-)
+from homeassistant import config_entries
 from homeassistant.components.econnect_alarm.const import DOMAIN
 
 
-async def test_form(hass):
-    """Test we get the form."""
-    await setup.async_setup_component(hass, "persistent_notification", {})
-    result = await hass.config_entries.flow.async_init(
+async def test_form_fields(hass):
+    """Test we get the form with the fields we expect."""
+    form = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
-    assert result["type"] == "form"
-    assert result["errors"] == {}
+    assert form["type"] == "form"
+    assert form["step_id"] == "user"
+    assert form["errors"] == {}
+    assert form["data_schema"].schema["username"] == str
+    assert form["data_schema"].schema["password"] == str
+    assert form["data_schema"].schema["domain"] == str
 
-    with patch(
-        "homeassistant.components.econnect_alarm.config_flow.PlaceholderHub.authenticate",
-        return_value=True,
-    ), patch(
-        "homeassistant.components.econnect_alarm.async_setup", return_value=True
-    ) as mock_setup, patch(
-        "homeassistant.components.econnect_alarm.async_setup_entry",
-        return_value=True,
-    ) as mock_setup_entry:
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-        await hass.async_block_till_done()
 
-    assert result2["type"] == "create_entry"
-    assert result2["title"] == "Name of the device"
-    assert result2["data"] == {
-        "host": "1.1.1.1",
+@patch("homeassistant.components.econnect_alarm.async_setup", return_value=True)
+@patch("homeassistant.components.econnect_alarm.async_setup_entry", return_value=True)
+@patch("homeassistant.components.econnect_alarm.config_flow.ElmoClient")
+async def test_form_submit_successful(mock_client, mock_setup_entry, mock_setup, hass):
+    """Test a valid form is properly submitted."""
+    form = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    result = await hass.config_entries.flow.async_configure(
+        form["flow_id"],
+        {
+            "username": "test-username",
+            "password": "test-password",
+            "domain": "test-domain",
+        },
+    )
+    await hass.async_block_till_done()
+
+    assert result["type"] == "create_entry"
+    assert result["title"] == "E-connect Alarm"
+    assert result["data"] == {
         "username": "test-username",
         "password": "test-password",
+        "domain": "test-domain",
     }
+    # Check HA setup
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
-
-
-async def test_form_invalid_auth(hass):
-    """Test we handle invalid auth."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.econnect_alarm.config_flow.PlaceholderHub.authenticate",
-        side_effect=InvalidAuth,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_auth"}
-
-
-async def test_form_cannot_connect(hass):
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "homeassistant.components.econnect_alarm.config_flow.PlaceholderHub.authenticate",
-        side_effect=CannotConnect,
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {
-                "host": "1.1.1.1",
-                "username": "test-username",
-                "password": "test-password",
-            },
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "cannot_connect"}
+    # Check Client initialization during validation
+    assert ("https://connect.elmospa.com",) == mock_client.call_args.args
+    assert {"domain": "test-domain"} == mock_client.call_args.kwargs
+    client = mock_client()
+    assert client.auth.call_count == 1
+    assert ("test-username", "test-password") == client.auth.call_args.args
