bbff4c3dbe6a6472713c1609742e7e307ce6de06
xtimmy86x <76836856+xtimmy86x@users.noreply.github.com>
2023-09-25 17:47:39 +0200

    Update test_helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,7 +1,7 @@
 import pytest
 
 from custom_components.elmo_iess_alarm.exceptions import InvalidAreas
-from custom_components.elmo_iess_alarm.helpers import parse_areas_config
+from custom_components.elmo_iess_alarm.helpers import generate_entity_name, parse_areas_config
 
 
 def test_parse_areas_config_valid_input():
@@ -31,3 +31,30 @@ def test_parse_areas_config_raises_value_error():
 
 def test_parse_areas_config_whitespace():
     assert parse_areas_config(" 3 , 4 ") == [3, 4]
+
+def test_generate_entity_name_empty():
+    class MockConfigEntry:
+        def __init__(self):
+            self.data = {
+                "username": "test_user",
+            }
+    config_entry = MockConfigEntry()
+    assert generate_entity_name(config_entry) == "elmo_iess_alarm test_user"
+
+def test_generate_entity_name_with_name():
+    class MockConfigEntry:
+        def __init__(self):
+            self.data = {
+                "username": "test_user",
+            }
+    config_entry = MockConfigEntry()
+    assert generate_entity_name(config_entry, "window") == "elmo_iess_alarm test_user window"
+
+def test_generate_entity_name_with_none():
+    class MockConfigEntry:
+        def __init__(self):
+            self.data = {
+                "username": "test_user",
+            }
+    config_entry = MockConfigEntry()
+    assert generate_entity_name(config_entry, None) == "elmo_iess_alarm test_user"
