35406f8a28080fd3ea5c80b1d1b96f7d0ee5b0d6
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2023-09-27 11:46:09 +0000

    refactor!: generate_entity_name now generates a valid entity_id

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,7 +3,7 @@ from homeassistant.core import valid_entity_id
 
 from custom_components.elmo_iess_alarm.exceptions import InvalidAreas
 from custom_components.elmo_iess_alarm.helpers import (
-    generate_entity_name,
+    generate_entity_id,
     parse_areas_config,
 )
 
@@ -38,46 +38,46 @@ def test_parse_areas_config_whitespace():
 
 
 def test_generate_entity_name_empty(config_entry):
-    entity_id = generate_entity_name(config_entry)
-    assert entity_id == "elmo_iess_alarm.test_user"
+    entity_id = generate_entity_id(config_entry)
+    assert entity_id == "elmo_iess_alarm.elmo_iess_alarm_test_user"
     assert valid_entity_id(entity_id)
 
 
 def test_generate_entity_name_with_name(config_entry):
-    entity_id = generate_entity_name(config_entry, "window")
-    assert entity_id == "elmo_iess_alarm.test_user_window"
+    entity_id = generate_entity_id(config_entry, "window")
+    assert entity_id == "elmo_iess_alarm.elmo_iess_alarm_test_user_window"
     assert valid_entity_id(entity_id)
 
 
 def test_generate_entity_name_with_none(config_entry):
-    entity_id = generate_entity_name(config_entry, None)
-    assert entity_id == "elmo_iess_alarm.test_user"
+    entity_id = generate_entity_id(config_entry, None)
+    assert entity_id == "elmo_iess_alarm.elmo_iess_alarm_test_user"
     assert valid_entity_id(entity_id)
 
 
 def test_generate_entity_name_empty_system(config_entry):
     config_entry.data["system_name"] = "Home"
-    entity_id = generate_entity_name(config_entry)
-    assert entity_id == "elmo_iess_alarm.home"
+    entity_id = generate_entity_id(config_entry)
+    assert entity_id == "elmo_iess_alarm.elmo_iess_alarm_home"
     assert valid_entity_id(entity_id)
 
 
 def test_generate_entity_name_with_name_system(config_entry):
     config_entry.data["system_name"] = "Home"
-    entity_id = generate_entity_name(config_entry, "window")
-    assert entity_id == "elmo_iess_alarm.home_window"
+    entity_id = generate_entity_id(config_entry, "window")
+    assert entity_id == "elmo_iess_alarm.elmo_iess_alarm_home_window"
     assert valid_entity_id(entity_id)
 
 
 def test_generate_entity_name_with_none_system(config_entry):
     config_entry.data["system_name"] = "Home"
-    entity_id = generate_entity_name(config_entry, None)
-    assert entity_id == "elmo_iess_alarm.home"
+    entity_id = generate_entity_id(config_entry, None)
+    assert entity_id == "elmo_iess_alarm.elmo_iess_alarm_home"
     assert valid_entity_id(entity_id)
 
 
 def test_generate_entity_name_with_spaces(config_entry):
     config_entry.data["system_name"] = "Home Assistant"
-    entity_id = generate_entity_name(config_entry)
-    assert entity_id == "elmo_iess_alarm.home_assistant"
+    entity_id = generate_entity_id(config_entry)
+    assert entity_id == "elmo_iess_alarm.elmo_iess_alarm_home_assistant"
     assert valid_entity_id(entity_id)
