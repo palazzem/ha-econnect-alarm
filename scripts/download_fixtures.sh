a8125980a9a2166cb0d8f2cfb03c267d33d3139e
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2026-01-16 15:35:35 +0100

    fix: init scripts are compatible in OSX and Linux

--- scripts/download_fixtures.sh
+++ scripts/download_fixtures.sh
@@ -19,7 +19,7 @@ fi
 
 # Variables
 DOWNLOAD_FOLDER=$(mktemp -d)
-HASS_TESTS_FOLDER=$DOWNLOAD_FOLDER/core-$VERSION/tests/
+HASS_TESTS_FOLDER="$DOWNLOAD_FOLDER/core-$VERSION/tests/"
 
 # Remove previous folder if exists
 if [ -d "tests/hass" ]; then
@@ -29,17 +29,17 @@ fi
 
 # Download HASS version
 echo "Downloading Home Assistant $VERSION in $DOWNLOAD_FOLDER"
-curl -L https://github.com/home-assistant/core/archive/refs/tags/$VERSION.tar.gz -o $DOWNLOAD_FOLDER/$VERSION.tar.gz
+curl -L "https://github.com/home-assistant/core/archive/refs/tags/$VERSION.tar.gz" -o "$DOWNLOAD_FOLDER/$VERSION.tar.gz"
 
 # Extract HASS fixtures and tests helpers, excluding all components and actual tests
 echo "Extracting tests/ folder from $VERSION.tar.gz"
-tar -C $DOWNLOAD_FOLDER --exclude='*/components/*' --exclude='*/pylint/*' -xf $DOWNLOAD_FOLDER/$VERSION.tar.gz core-$VERSION/tests
-find $HASS_TESTS_FOLDER -type f -name "test_*.py" -delete
+tar -C "$DOWNLOAD_FOLDER" --exclude='*/components/*' --exclude='*/pylint/*' -xf "$DOWNLOAD_FOLDER/$VERSION.tar.gz" "core-$VERSION/tests"
+find "$HASS_TESTS_FOLDER" -type f -name "test_*.py" -delete
 
 # Recursively find and update imports
-find $HASS_TESTS_FOLDER -type f -exec sed -i 's/from tests\./from tests.hass./g' {} +
-mv $HASS_TESTS_FOLDER/conftest.py $HASS_TESTS_FOLDER/fixtures.py
+find "$HASS_TESTS_FOLDER" -type f -exec perl -pi -e 's/from tests\./from tests.hass./g' {} +
+mv "$HASS_TESTS_FOLDER/conftest.py" "$HASS_TESTS_FOLDER/fixtures.py"
 
 # Copy Home Assistant fixtures
-mv $HASS_TESTS_FOLDER ./tests/hass
+mv "$HASS_TESTS_FOLDER" ./tests/hass
 echo "Home Assistant $VERSION tests are now in tests/hass/"
